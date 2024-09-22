4932cc1d39e2307fb6b917685e9024c3ae00833d
ritaban06 <ghoshritabancollege@gmail.com>
2024-09-22 12:27:12 +0530

    Modified tailwind-config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+theme: {
+  extend: {},
+  container: {
+    center: true,
   },
-  plugins: [],
+},
+plugins: [],
 }
-
